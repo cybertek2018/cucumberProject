43c29e88ab0723b31ae382a23f21c91a375ca734
cybertek2018 <37963733+cybertek2018@users.noreply.github.com>
2018-04-26 10:26:15 -0500

    Update CucumberFirst.java

--- src/test/java/cucumberProject/CucumberFirst.java
+++ src/test/java/cucumberProject/CucumberFirst.java
@@ -4,6 +4,8 @@ public class CucumberFirst {
 	
 	public void firstMethod(){
 		System.out.println("this is the first cucumber method in maven project");
+				System.out.println("this is the first cucumber method in maven project 2");
+
 	}
 
 }
